62904ac1bf6356aba1e75b6bd46fb2d11d37660d
unknown <zzser15963@gmail.com>
2020-05-14 01:05:32 +0800

    Initial Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbSidebarService, NbMenuItem } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-demo';
+
+  constructor(private sidebarService: NbSidebarService) {
+  }
+
+  public topics: NbMenuItem[] = [
+    {
+      title: '題目列表',
+      expanded: true,
+      children: [
+        {
+          title: 'Python第一次上課 20/09',
+        },
+        {
+          title: 'Python第二周09/27',
+        },
+        {
+          title: 'Python第二周09/30',
+        },
+      ],
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbA11yModule, NbSidebarModule, NbActionsModule, NbMenuModule, NbCardModule, NbSelectModule, NbAccordionModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbSidebarService } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule,
+    NbActionsModule,
+    NbCardModule,
+    NbSelectModule,
+    NbAccordionModule,
+    NbMenuModule.forRoot(),
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
